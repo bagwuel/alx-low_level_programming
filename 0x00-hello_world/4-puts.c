71f3133c63d244f8fab4c766ee00aa8434130702
bagwuel <mbagwu39@gmail.com>
2023-03-16 09:02:54 -0700

    completed tasks 2 - 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * The main function prints "Programming is like building a multilingual puzzle using the puts funtion.
+ * The return value is (0)
+ */
+
+int	main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
