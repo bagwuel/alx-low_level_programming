b633988b03f6984746f3f8cf38e174045edc26bd
bagwuel <mbagwu39@gmail.com>
2023-03-22 13:53:34 -0700

    corrected task 3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,9 +16,7 @@ int largest_number(int a, int b, int c)
 		largest = a;
 	else if (b > c && b > a)
 		largest = b;
-	else if (c > b);
+	else if (c > b && c > a);
 		largest = c;
-	else
-		largest = b;
 	return (largest);
 }
