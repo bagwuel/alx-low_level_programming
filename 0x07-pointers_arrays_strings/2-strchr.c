b5c12d7445bed74af0f77ab8374ad5b3989ec41e
bagwuel <mbagwu39@gmail.com>
2023-04-03 17:55:16 +0000

    correct t2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,11 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
+	while (*s)
 	{
-		if (s[i] == c)
-			return (&s[i]);
-		i++;
+		if (*s == c)
+			return (s);
+		s++;
 	}
 	return (0);
 }
