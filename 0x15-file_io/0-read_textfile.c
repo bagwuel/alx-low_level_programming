4603f4b072d692d614d38934b117dbd42302e134
bagwuel <mbagwu39@gmail.com>
2023-05-08 14:03:24 +0000

    correct task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,9 +28,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	rb = read(fd, buffer, letters);
 	wb = write(STDOUT_FILENO, buffer, rb);
+	close (fd);
+	free(buffer);
 	if (wb != rb)
 		return (0);
-	close(fd);
-	free(buffer);
 	return (wb);
 }
