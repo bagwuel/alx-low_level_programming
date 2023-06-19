3f7b630affc8ff3c47b2ded52610c5bfdc3ca93c
bagwuel <mbagwu39@gmail.com>
2023-06-19 21:46:37 +0000

    complete task 2

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two numbers together
+ * @a: first number
+ * @b: secon number
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - finds the difference between two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a - b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a / b
+ */
+
+int div(int a, int b)
+{
+	if (b != 0)
+		return (0);
+	return (a / b);
+}
+
+/**
+ * mod - finds the modulo two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+	if (b != 0)
+		return (0);
+	return (a % b);
+}
